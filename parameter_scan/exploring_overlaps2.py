f41d8f056e23fca2cea94eef2975cf1b32a64d9e
Manuel Lera Ramirez <manulera14@gmail.com>
2023-04-18 23:42:23 +0100

    removed old parameter scans

--- parameter_scan/exploring_overlaps2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from matplotlib import pyplot as plt
-
-data = np.genfromtxt(f'runs_overlaps_2/scan/run0282/solution.txt',delimiter=',')
-
-plt.figure()
-plt.plot(data[-1,:])
-plt.show()
-
--- parameter_scan/run_all.sh
+++
@@ -1,9 +0,0 @@
-bash run_scan.sh runs_coop1
-bash run_scan.sh runs_coop2
-bash run_scan.sh runs_special_coop1
-bash run_scan.sh runs_special_coop2
-
-bash run_analysis.sh runs_coop1
-bash run_analysis.sh runs_coop2
-bash run_analysis.sh runs_special_coop1
-bash run_analysis.sh runs_special_coop2
